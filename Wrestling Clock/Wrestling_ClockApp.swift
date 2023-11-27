909fe3f48ef2b4ffae5980efdcd9f18ba5459056
Phillip Mantatsky <pmantatsky1@bsd220.org>
2023-11-27 09:57:46 -0600

    Initial Commit

---
+++ Wrestling Clock/Wrestling_ClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wrestling_ClockApp.swift
+//  Wrestling Clock
+//
+//  Created by Phillip Mantatsky on 11/27/23.
+//
+
+import SwiftUI
+
+@main
+struct Wrestling_ClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
